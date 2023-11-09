c0ad2558c5ba7b19bdd191782599fae90c6054fb
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-11-09 15:27:31 +0100

    Create Single Cell

---
+++ Projekt Inzynierski/Assets/MazeCell.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCell : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _wallRight;
+
+    [SerializeField]
+    private GameObject _wallLeft;
+
+    [SerializeField]
+    private GameObject _wallFront;
+
+    [SerializeField]
+    private GameObject _wallBack;
+
+    [SerializeField]
+    private GameObject _unvisitedCell;
+
+    public bool IsVisited { get; private set; }
+
+    public void Visit()
+    {
+        IsVisited = true;
+        _unvisitedCell.SetActive(false);
+    }
+
+    public void ClearLeft()
+    {
+        _wallLeft.SetActive(false);
+    }
+
+    public void ClearBack()
+    {
+        _wallBack.SetActive(false);
+    }
+
+    public void ClearFront()
+    {
+        _wallFront.SetActive(false);
+    }
+
+    public void ClearRight()
+    {
+        _wallRight.SetActive(false);
+    }
+
+}
