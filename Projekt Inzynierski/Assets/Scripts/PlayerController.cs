4db11bb48a411acf12813ac5afdfb48eed22bb5e
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-12-04 15:41:13 +0100

    Create timer

--- Projekt Inzynierski/Assets/Scripts/PlayerController.cs
+++ Projekt Inzynierski/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5.0f;
-    void Start()
+    
+    public Timer timer;
+
+    private void Start()
     {
 
+        timer = GetComponent<Timer>();
     }
 
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         transform.Translate(move * speed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("EndCell"))
+        {
+            Debug.Log("Finish");
+            timer.StopStopwatch();
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("StartCell"))
+        {
+            Debug.Log("Start");
+            timer.StartStopwatch();
+        }
     }
 
 }
---
+++ Projekt Inzynierski/Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using TMPro;
+using System.Globalization;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Component")]
+    public TextMeshProUGUI timerText;
+
+    private bool isTiming = false;
+    private float elapsedTime = 0f;
+
+    void Update()
+    {
+        if (isTiming)
+        {
+            elapsedTime += Time.deltaTime;
+            UpdateTimeText();
+        }
+    }
+
+    void UpdateTimeText()
+    {
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        string timeString = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = timeString;
+    }
+
+    public void StartStopwatch()
+    {
+        isTiming = true;
+    }
+
+    public void StopStopwatch()
+    {
+        isTiming = false;
+    }
+
+
+    /*
+    [Header("Timer Settings")]
+    public static float currentTime;
+    public bool countDown;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText.text = "Time: 0.0";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+        CultureInfo culture = new CultureInfo("en-US");
+        timerText.text = "Time: " + currentTime.ToString("0.0", culture);
+
+    }
+     */
+
+
+
+
+
+
+
+}
